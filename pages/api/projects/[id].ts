36837bd6fae3e6a32e82c3a8d8ae384f8b18a269
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-01-01 11:26:41 +0900

    プロジェクト削除APIをちょっとリファクタ

--- pages/api/projects/[id].ts
+++ pages/api/projects/[id].ts
@@ -6,23 +6,19 @@ function parseIdFromQuery(req: NextApiRequest): number {
   return Array.isArray(id) ? Number(id[0]) : Number(id)
 }
 
-function fetchProject(id: number) {
-  return prisma.project.findFirst({ where: { id } })
-}
-
-function deleteProject(id: number) {
-  return prisma.project.delete({ where: { id } })
+async function deleteProject(id: number, res: NextApiResponse) {
+  await prisma.project.delete({ where: { id } })
+  return res.status(200).json({ message: 'success' })
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const id = parseIdFromQuery(req)
-  const project = await fetchProject(id)
+  const project = await prisma.project.findFirst({ where: { id } })
   if (!project) return res.status(404).json({ message: 'not found' })
 
   switch (req.method) {
     case 'DELETE':
-      await deleteProject(project.id)
-      return res.status(200).json({ message: 'success' })
+      return await deleteProject(id, res)
     default:
       return res.status(405).json({ message: 'method not allowed' })
   }
